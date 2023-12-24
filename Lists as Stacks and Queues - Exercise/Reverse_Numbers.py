94c6dda5f472294f2652f81baddc92dd221ae46b
Yashov <dimitaryashov@yahoo.co.uk>
2023-12-24 20:20:07 +0200

    New option to solve

--- Lists as Stacks and Queues - Exercise/Reverse_Numbers.py
+++ Lists as Stacks and Queues - Exercise/Reverse_Numbers.py
@@ -3,6 +3,6 @@ from collections import deque
 numbers = deque(input().split())
 
 # for _ in range(len(numbers)):
-#     print(numbers.pop(), end=" ")
+#     print(numbers.pop(), end=' ')
 numbers.reverse()
-print(" ".join(numbers))
+print(' '.join(numbers))
--- Lists as Stacks and Queues - Lab/Water_Dispenser.py
+++ Lists as Stacks and Queues - Lab/Water_Dispenser.py
@@ -1,30 +1,60 @@
+# from collections import deque
+#
+# people = deque()
+# water_quantity = int(input())
+# name = input()
+#
+# # while name != "Start":
+# #     people.append(name)
+# #     name = input()
+#
+# command = input()
+
+# while command != "End":
+#     data = command.split()
+# if len(data) == 1:
+#     litters_wanted = int(data[0])
+#
+#     if water_quantity >= litters_wanted:
+#         water_quantity -= litters_wanted
+#         person = people.popleft()
+#         print(f"{person} got water")
+#     else:
+#         person = people.popleft()
+#         print(f"{person} must wait")
+# else:
+#     litters_refill = int(data[1])
+#     water_quantity += litters_refill
+# command = input()
+# print(f"{water_quantity} litters_wanted left")
+
 from collections import deque
 
-people = deque()
+queue = deque()
 water_quantity = int(input())
 name = input()
 
-while name != "Start":
-    people.append(name)
+while name != 'Start':
+    queue.append(name)
     name = input()
 
 command = input()
 
 while command != "End":
-    data = command.split()
-    if len(data) == 1:
-        litters_wanted = int(data[0])
+    liters = command.split()
 
-        if water_quantity >= litters_wanted:
-            water_quantity -= litters_wanted
-            person = people.popleft()
-            print(f"{person} got water")
-        else:
-            person = people.popleft()
-            print(f"{person} must wait")
+    if "refill" in command:
+        liters_refill = int(liters[1])
+        water_quantity += liters_refill
+
     else:
-        litters_refill = int(data[1])
-        water_quantity += litters_refill
+        liters = int(command)
+        if liters <= water_quantity:
+            water_quantity-= liters
+            print(f"{queue.popleft()} got water")
+        else:
+            print(f'{queue.popleft()} must wait')
+
     command = input()
 
 print(f"{water_quantity} liters left")
