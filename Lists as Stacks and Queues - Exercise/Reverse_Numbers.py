67d01a38a1ef83a76c806eb6ea300f3456b63367
Yashov <dimitaryashov@yahoo.co.uk>
2023-12-26 13:03:14 +0200

    Update Reverse_Numbers.py

--- Lists as Stacks and Queues - Exercise/Reverse_Numbers.py
+++ Lists as Stacks and Queues - Exercise/Reverse_Numbers.py
@@ -1,8 +1,15 @@
 from collections import deque
 
-numbers = deque(input().split())
+
+#solution 1
+# numbers = deque(input().split())
 
 # for _ in range(len(numbers)):
 #     print(numbers.pop(), end=' ')
+
+
+#solution 2
+numbers = deque(input().split())
+
 numbers.reverse()
 print(' '.join(numbers))
