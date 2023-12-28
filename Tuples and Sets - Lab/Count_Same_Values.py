21bc50cfb81670c952c87e3773a435a1bf8b0c14
Yashov <dimitaryashov@yahoo.co.uk>
2023-12-28 17:43:24 +0200

    Nothing Special

--- Tuples and Sets - Lab/Count_Same_Values.py
+++ Tuples and Sets - Lab/Count_Same_Values.py
@@ -1,11 +1,19 @@
-nums = tuple(map(float, input().split()))
+nums = tuple([float(num) for num in input().split()])
+#nums = tuple(map(float, input().split()))
 
 occ = {}
 
-for el in nums:
-    if el not in occ:
-        occ[el] = 0
-    occ[el] += 1
+# for el in nums:
+#     if el not in occ:
+#         occ[el] = 0
+#     occ[el] += 1
+#
+# for num, count in occ.items():
+#     print(f"{num} - {count} times")
 
-for num, count_ in occ.items():
-    print(f"{num} - {count_} times")
+
+for el in nums:
+    occ[el] = nums.count(el)
+
+for num, count in occ.items():
+    print(f"{num} - {count} times")
--- Tuples and Sets - Lab/Students'_Grades.py
+++ Tuples and Sets - Lab/Students'_Grades.py
@@ -9,10 +9,23 @@ for _ in range(n):
     name, grade = tuple(input().split())
     grade = float(grade)
 
-    # if name not in students_grades:
-    #     students_grades[name] = []
     students_grades[name].append(grade)
 
 for name, grades in students_grades.items():
-    avg = mean(grades)
+    avg = sum(grades) / len(grades)
     print(f"{name} -> {' '.join([str(f'{grade:.2f}') for grade in grades])} (avg: {avg:.2f})")
+
+
+# students_grades = {}
+#
+# for _ in range(n):
+#     name, grade = tuple(input().split())
+#     grade = float(grade)
+#
+#     if name not in students_grades:
+#         students_grades[name] = []
+#     students_grades[name].append(grade)
+#
+# for name, grades in students_grades.items():
+#     avg = sum(grades) / len(grades)
+#     print(f"{name} -> {' '.join([str(f'{grade:.2f}') for grade in grades])} (avg: {avg:.2f})")
