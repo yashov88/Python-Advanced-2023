e686c3b6707008674aee0458f8fb54a8283ba015
Yashov <dimitaryashov@yahoo.co.uk>
2024-01-11 18:55:59 +0200

    Indexing problems

---
+++ Multidimensional Lists - Exercise 1/Bombs.py
@@ -0,0 +1,41 @@
+def check_indices(row1, col1, row2, col2):
+    if matrix[row1][col1]:
+        matrix[row1][col1] -= matrix[row2][col2]
+
+
+rows = int(input())
+matrix = [[int(el) for el in input().split()] for _ in range(rows)]
+bombs = input().split()
+sum_alive_cells = 0
+
+for idx in bombs:
+    row, col = [int(el) for el in idx.split(',')]
+
+    if matrix[row][col]:
+
+        check_indices(row - 1, col - 1, row, col)
+        check_indices(row - 1, col, row, col)
+        check_indices(row - 1, col + 1, row, col)
+        check_indices(row, col + 1, row, col)
+        check_indices(row + 1, col + 1, row, col)
+        check_indices(row + 1, col, row, col)
+        check_indices(row + 1, col - 1, row, col)
+        check_indices(row, col - 1, row, col)
+
+        matrix[row][col] = 0
+
+alive_cells = 0
+
+for el_idx in matrix:
+    for el in el_idx:
+        if el > 0:
+            alive_cells += 1
+            sum_alive_cells += el
+
+print(f'"Alive cells: {alive_cells}')
+print(f'"Sum: {sum_alive_cells}')
+
+print(*matrix, sep='\n')
+
+
+# indices = all(all(el == matrix[idx] for el in matrix) for idx in range(len(bombs)))
