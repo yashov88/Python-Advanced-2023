9e3f74c716c48b24b89b5cda0e0e7ee2752f5862
Yashov <dimitaryashov@yahoo.co.uk>
2024-01-27 11:33:35 +0200

    Update Bombs.py

--- Multidimensional Lists - Exercise 1/Bombs.py
+++ Multidimensional Lists - Exercise 1/Bombs.py
@@ -1,33 +1,22 @@
-def check_indices(row1, col1, row2, col2):
-    if matrix[row1][col1] > 0:
-        matrix[row1][col1] -= matrix[row2][col2]
-    else:
-        matrix[row2][col2] -= matrix[row1][col1]
-
-
-rows = int(input())
-matrix = [[int(el) for el in input().split()] for _ in range(rows)]
-bombs = input().split()
-# indices = all(all(el == matrix[idx] for el in matrix) for idx in range(len(bombs)))
-
-for idx in bombs:
-    row, col = [int(el) for el in idx.split(',')]
-
-    if matrix[row][col]:
-
-        check_indices(row - 1, col - 1, row, col)
-        check_indices(row - 1, col, row, col)
-        check_indices(row - 1, col + 1, row, col)
-        check_indices(row, col + 1, row, col)
-        check_indices(row + 1, col + 1, row, col)
-        check_indices(row + 1, col, row, col)
-        check_indices(row + 1, col - 1, row, col)
-        check_indices(row, col - 1, row, col)
-
-        matrix[row][col] = 0
-
+n = int(input())
+matrix = [[int(el) for el in input().split()] for _ in range(n)]
+bombs = [info.split(",") for info in input().split()]
+explode_positions = ((-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1))
 alive_cells = 0
 sum_alive_cells = 0
+
+for bomb in bombs:
+    row = int(bomb[0])
+    col = int(bomb[1])
+    value = matrix[row][col]
+    if value > 0:
+        matrix[row][col] = 0
+        for idx in explode_positions:
+            current_row = row + idx[0]
+            current_col = col + idx[1]
+            if 0 <= current_row < n and 0 <= current_col < n:
+                if matrix[current_row][current_col] > 0:
+                    matrix[current_row][current_col] -= value
 
 for el_idx in matrix:
     for el in el_idx:
@@ -37,5 +26,4 @@ for el_idx in matrix:
 
 print(f'"Alive cells: {alive_cells}')
 print(f'"Sum: {sum_alive_cells}')
-
-print(*matrix, sep='\n')
+[print(*elements) for elements in matrix]
